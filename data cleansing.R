1f7eb68fb847d924edcfab10f7e5cf0e0bf267f5
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-22 17:07:54 +1000

    data explorasion

--- data cleansing.R
+++ data cleansing.R
@@ -10,3 +10,27 @@ propP[order(propP,decreasing=T)[1:10]]
 ## join small groups together
 detach(sales)
 sales <- sales[-which(is.na(sales$Quant)&is.na(sales$Val)),]
+
+## Percent of NA in VAL / QUANT by ID and PROD
+nnasQp <- tapply(sales$Quant,list(sales$Prod),function(x)sum(is.na(x)))
+propNAsQp <- nnasQp/table(sales$Prod)
+propNAsQp[order(propNAsQp,decreasing=T)[1:10]]
+
+sales <- sales[!sales$Prod %in% c('p2442','p2443'),]
+
+nlevels(sales$Prod)
+sales$Prod <- factor(sales$Prod)
+nlevels(sales$Prod)
+
+nnasQs <- tapply(sales$Quant,list(sales$ID),function(x)sum(is.na(x)))
+propNAsQs <- nnasQs/table(sales$ID)
+propNAsQs[order(propNAsQs,decreasing = T)[1:10]]
+
+nnasVp <- tapply(sales$Val,list(sales$Prod),function(x)sum(is.na(x)))
+propNAsVp<-nnasVp/table(sales$Prod)
+propNAsVp[order(propNAsVp,decreasing = T)[1:10]]
+
+nnasVs <- tapply(sales$Val,list(sales$ID),function(x)sum(is.na(x)))
+propNAsVs<-nnasVs/table(sales$ID)
+propNAsVs[order(propNAsVs,decreasing = T)[1:10]]
+##178
