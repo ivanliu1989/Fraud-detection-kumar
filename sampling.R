eefb068fde6d4517eb539f3db441f939de2f11bd
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-24 11:34:24 +1000

    over sampling

--- sampling.R
+++ sampling.R
@@ -1,1 +1,7 @@
-## stratified sampling
+#imbalanced sample
+data(iris)
+data <- iris[,c(1,2,5)]
+data$Species <- factor(ifelse(data$Species == 'setosa','rare','common'))
+newData <- SMOTE(Species~.,data,perc.over=600)
+table(newData$Species)
+table(data$Species)
