9fef9e45e89c8c7a3cd4d64357e56f1fae8940d3
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-24 10:09:36 +1000

    Unsupervised approaches

--- Unsupervised approaches.R
+++ Unsupervised approaches.R
@@ -44,3 +44,24 @@ info <- attr(bp.res,'itsInfo')
 PTs.bp <- aperm(array(unlist(info),dim=c(length(info[[1]]),2,3)),c(1,3,2))
 PRcurve(PTs.bp[,,1],PTs.bp[,,2],main='PR curve',avg='vertical')
 CRchart(PTs.bp[,,1],PTs.bp[,,2],main='Cumulative Recall curve',avg='vertical')
+
+## Local Outlier Factors
+ho.LOF <-function(form, train,test,k,...){
+    ntr<-nrow(train)
+    all <- rbind(train,test)
+    N<-nrow(all)
+    ups<-split(all$Uprice,all$Prod)
+    r<-list(length=ups)
+    for(u in seq(along=ups))
+        r[[u]]<-if(NROW(ups[[u]])>3)
+            lofactor(ups[[u]],min(k,NROW(ups[[u]]) %/% 2))
+    else if (NROW(ups[[u]])) rep(0,NROW(ups[[u]]))
+    else NULL
+    all$lof <- vector(length=N)
+    split(all$lof,all$Prod)<-r
+    all$lof[which(!(is.infinite(all$lof)|is.nan(all$lof)))]<-
+        SoftMax(all$lof[which(!(is.infinite(all$lof)|is.nan(all$lof)))])
+    structure(evalOutlierRanking(test,order(all[(ntr+1):N,'lof'],decreasing=T),...),
+              itInfo=list(preds=all[(ntr+1):N,'lof'],
+                          trues=ifelse(test$Insp=='fraud',1,0)))
+}
